b4823c5b64243b0ad4081e91e657456d78b23cc0
sarikabanu <sarika.b@shrigowri.com>
2017-11-10 15:11:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-exports.myDateTime = function(){
-    return Date();
-}
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+
+http.createServer(function(req,res){
+var q = url.parse(req.url,true)
+var filename = "."+q.pathname;
+console.log('filename'+filename)
+fs.readFile(filename,function(err,data){
+    if(err)
+    {
+        res.writeHead(404,{'Content-type':'text/html'});
+        return res.end("404 not found")
+    }
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write(data);
+        return res.end()
+});
+
+}).listen(3000);

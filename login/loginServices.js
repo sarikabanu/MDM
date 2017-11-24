332024f63b4ec81bfc0632fedf600e3c19e87122
sarikabanu <sarika.b@shrigowri.com>
2017-11-24 11:32:33 +0530

    Update loginServices.js

--- login/loginServices.js
+++ login/loginServices.js
@@ -6,11 +6,11 @@ function Services() {
   this.adminlogin = function (req, res) {
        let mail_id = req.body.mail_id
         let password = req.body.password
-        let role_id = req.body.role_id
+//         let role_id = req.body.role_id
          if (!mail_id&&!password&&!role_id) {
             res.send({ status: 0, message: 'mail_id , password and role_id is required', content: '' })
           }else {
-            if(role_id==1||role_id==0){
+//             if(role_id==1||role_id==0){
             console.log('as admin ')
              loginService.loginAdmin(req.body,function(error,response) {
                 console.log('response  ' + response)
@@ -20,10 +20,10 @@ function Services() {
                     res.send({ status: 0, message:error.toString(), content: '' })
                 }
             });
-         } else{
-             logging.LoggingFunction('adminlogin','sorry you cannot login');
-             res.send({ status: 0, message:'sorry you cannot login', content: '' }) 
-         }
+//          } else{
+//              logging.LoggingFunction('adminlogin','sorry you cannot login');
+//              res.send({ status: 0, message:'sorry you cannot login', content: '' }) 
+//          }
     }
 },
 
